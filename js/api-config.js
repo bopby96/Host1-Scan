5fc4c46381865405e1473b45154d6db39370c57b
bopby96 <75601820+bopby96@users.noreply.github.com>
2026-06-04 09:33:29 +0700

    Add files via upload

--- js/api-config.js
+++ js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycby4TPn7tgQEGD8X8K6BbvD0XEpa92pc2kyyqW2lFisvGdZime8Zfe7n3usXcpN054IrJw/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycby1dFXYXK06OVq7WuXMiOF8pDAh02ykId9HRR2zuHhqJQKiu3cODet1mKh_32TRmi8V5w/exec';
